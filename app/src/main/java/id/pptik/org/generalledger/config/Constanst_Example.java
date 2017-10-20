0763449714c8a2ae3a21b4784a77cc2361bd3158
yusfia <yusfia.hafidz@gmail.com>
2017-10-20 18:40:51 +0700

    Constants Example added

---
+++ app/src/main/java/id/pptik/org/generalledger/config/Constanst_Example.java
@@ -0,0 +1,50 @@
+package id.pptik.org.generalledger.config;
+
+/**
+ * Created by Hafid on 10/20/2017.
+ */
+
+public class Constanst_Example {
+
+    /**
+     * Created by Hafid on 9/26/2017.
+     */
+
+
+    //FTP const
+    public static final int FTP_PORT = 21;
+    public static final String FTP_BASE_ADDRESS = "";
+    public static final String FTP_USER = "";
+    public static final String FTP_PASSWORD = "";
+    public static final String FTP_HTTP_FILEADDRESS = "";
+
+    //REST API
+    public static final String PORT_REST_API = "";
+    public static final String BASE_URL_REST_API = "";//"http://167.205.7.227:" + PORT_REST_API + "/";//"http://generalledger.pptik.id/";
+    public static final String REST_REGISTER_USER = "";
+    public static final String REST_LOGIN_USER = "";
+    public static final String REST_GET_BARANG = "";
+    public static final String REST_POST_REPORT = "";
+    public static final String REST_UPLOAD_PHOTO = "";
+    public static final String REST_RETRIEVE_PHOTO = "";
+    public static final String REST_POST_REPORT_MULTIPLE = "";
+
+    //RMQ
+    public static final int PORT_RMQ = 0;
+    public static final String BASE_URL_RMQ = "";
+    public static final String RMQ_VIRTUAL_HOST = "";
+    public static final String RMQ_EXCHANGE_NAME = "";
+    public static final String RMQ_INSERT_TRANSACTION_ROUTE = "";
+    public static final String RMQ_USERNAME = "";
+    public static final String RMQ_PASSWORD = "";
+    public static final String RMQ_EXCHANGE_TYPE = "";
+    public static final String RMQ_GET_LAPORAN_TRANSACTION_ROUTE = "";
+
+    //LaporanGambar Action
+    public static final int TAKE_PICTURE = 0;
+    public static final int MAX_IMAGE_SIZE = 300;
+
+    //ACTION
+    public static final String BROADCAST_INSERT_RESULT = "";
+}
+
